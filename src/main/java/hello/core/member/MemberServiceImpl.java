6dc68dd9004771a2bb75fac8c6ce4b80fbd4652f
sungrolee <sirorong2@gmail.com>
2023-04-24 12:56:51 +0900

    회원 도메인 실행과 테스트2

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,8 +1,8 @@
 package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
-
-
+    //클래스 이름은 1개만 있을 경우 관용적으로 마지막에 Impl을 붙여줌
+    //실제 repository에 구현된 메소드를 실행하여 데이터 저장하는 부분 구현
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
